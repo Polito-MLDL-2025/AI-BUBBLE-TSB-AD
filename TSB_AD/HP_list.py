58837771245b35d32c56da3d0864c7a3ee162e78
Ciovino <tarantino.giovanbattista01@gmail.com>
2025-12-27 16:18:56 +0100

    fix: added hyperparameters for MultiVariate case

--- TSB_AD/HP_list.py
+++ TSB_AD/HP_list.py
@@ -84,6 +84,13 @@ Multi_algo_HP_dict = {
     },    
     'OFA': {
         'win_size': [50, 100, 150]
+    },
+    'Chronos_2_AE': {
+        'slidingWindow': [50, 100],
+        'head_type': ['ae', 'vae'],
+        'latent_dim': [32, 64],
+        'lr': [1e-3, 1e-4],
+        'epochs': [5, 10]
     } 
 }
 
@@ -113,7 +120,7 @@ Optimal_Multi_algo_HP_dict = {
     'TimesNet': {'win_size': 96, 'lr': 0.0001},
     'FITS': {'win_size': 100, 'lr': 0.001},
     'OFA': {'win_size': 50},
-    'Chronos_2_AE': {'slidingWindow': 100, 'head_type': 'vae', 'epochs': 1}
+    'Chronos_2_AE': {'slidingWindow': 50, 'head_type': 'vae', 'latent_dim': 64, 'lr': 0.001, 'epochs': 10}
 }
 
 
@@ -239,7 +246,7 @@ Uni_algo_HP_dict = {
         'head_type': ['ae', 'vae'],
         'latent_dim': [32, 64],
         'lr': [1e-3, 1e-4],
-        'epochs': [5]
+        'epochs': [5, 10]
     }
 }
 
@@ -280,5 +287,5 @@ Optimal_Uni_algo_HP_dict = {
     'MOMENT_ZS': {'win_size': 64},
     'MOMENT_FT': {'win_size': 64},
     'M2N2': {},
-    'Chronos_2_AE': {'slidingWindow': 50, 'head_type': 'vae', 'latent_dim': 64, 'lr': 0.001, 'epochs': 5}
+    'Chronos_2_AE': {'slidingWindow': 50, 'head_type': 'vae', 'latent_dim': 64, 'lr': 0.001, 'epochs': 10}
 }
